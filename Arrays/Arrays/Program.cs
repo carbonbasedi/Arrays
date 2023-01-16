b5bdc8debe9bf9eaf75b8d4198c575a05c4cebf1
carbonbasedi <carbonbasedai@gmail.com>
2023-01-16 19:49:00 +0400

    Correction in 3rd task

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -49,30 +49,26 @@
 //Console.WriteLine($"{sumofarray / intNums.Length} - average of given numbers");
 
 
-//                                                                     3   -  Sorting algorithm in descending order
+//                                                                     3   -  Sorting algorithm in reverse order
 
 
-//Console.WriteLine("Insert 5 numbers you want them to be shown in descending order");
+//Console.WriteLine("Insert 5 numbers you want them to be shown in reverse order");
 
 //int[] natNums = new int[5];
 //for (int i = 0; i < natNums.Length; i++)
 //{
 //    natNums[i] = Convert.ToInt32(Console.ReadLine());
 //}
-//int temp = 0;
+//int temp;
 
-//Console.WriteLine("Given numbers in descending order");
+//Console.WriteLine("Given numbers in reverse order");
 //for (int i = 0; i < natNums.Length; i++)
 //{
 //    for (int j = i + 1; j < natNums.Length; j++)
 //    {
-//        if (natNums[i] < natNums[j])
-//        {
-//            temp = natNums[i];
-//            natNums[i] = natNums[j];
-//            natNums[j] = temp;
-//        }
-
+//        temp = natNums[i];
+//        natNums[i] = natNums[j];
+//        natNums[j] = temp;
 //    }
 //    Console.WriteLine(natNums[i]);
 //}
