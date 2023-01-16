bc0e969f551a5f45239f924b1872bc41df4c8084
carbonbasedi <carbonbasedai@gmail.com>
2023-01-16 15:28:11 +0400

    Arrays first iteration

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,58 @@
+﻿
+//1 - Sorting algorithm in ascending order
+
+//int[] natNums = { 45, 4546, 546, 878, 2, 87, 12, 2, 64, 7, 8, 6 };
+//int temp = 0;
+
+//Console.WriteLine("Given numbers in ascending order");
+//for (int i = 0; i < natNums.Length; i++)
+//{
+//    for (int j = i + 1; j < natNums.Length; j++)
+//    {
+//        if (natNums[i] > natNums[j])
+//        {
+//            temp = natNums[i];
+//            natNums[i] = natNums[j];
+//            natNums[j] = temp;
+//        }
+
+//    }
+//    Console.WriteLine(natNums[i]);
+//}
+
+
+//                                                                    2   -   Algorithm for finding average of given numbers in array.
+
+//int[] intnums = {1,2,3,4,5,6,7,8,9,10 };
+//int sumofarray = 0;
+
+//for (int i = 0; i < intnums.Length; i++)
+//{
+//    sumofarray += intnums[i];
+//}
+//Console.WriteLine($"{sumofarray} - sum of all given numbers");
+//Console.WriteLine($"{intnums.Length} - count of given numbers");
+//Console.WriteLine($"{sumofarray / intnums.Length} - average of given numbers");
+
+
+//                                                                     3   -  Sorting algorithm in descending order
+
+
+//int[] natNums = { 45, 4546, 546, 878, 2, 87, 12, 2, 64, 7, 8, 6 };
+//int temp = 0;
+
+//Console.WriteLine("Given numbers in descending order");
+//for (int i = 0; i < natNums.Length; i++)
+//{
+//    for (int j = i + 1; j < natNums.Length; j++)
+//    {
+//        if (natNums[i] < natNums[j])
+//        {
+//            temp = natNums[i];
+//            natNums[i] = natNums[j];
+//            natNums[j] = temp;
+//        }
+
+//    }
+//    Console.WriteLine(natNums[i]);
+//}
